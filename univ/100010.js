d27c16f3e59a7de6a4b974c997c10a998e0a45f1
ELSKER0305 <43704652+ELSKER0305@users.noreply.github.com>
2019-02-27 19:27:07 +0800

    Create 100010.js

---
+++ univ/100010.js
@@ -0,0 +1,17 @@
+var univ_list = {
+  school:"中国石油大学(华东)"，
+  id:"100010",
+  link: [ {
+    name: "石大主页",
+    url: "http://www.upc.edu.cn",
+  }, {
+    name:" 数字石大",
+    url:"https://cas.upc.edu.cn/cas/",
+  },{
+    name:"图书馆",
+    url:"http://library.upc.edu.cn/",
+  },{
+    name:"石大易班",
+    url:"http://yb.upc.edu.cn/#/",
+  }]
+};
